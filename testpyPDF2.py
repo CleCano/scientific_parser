2396e40d06f8b3132d4bad7572075fdc8365d72b
Clément LBB <le-bihannic-bertot@etud.univ-ubs.fr>
2023-02-16 14:35:04 +0100

    separation methodes + titre + author depuis metadata

--- testpyPDF2.py
+++ testpyPDF2.py
@@ -2,6 +2,38 @@ import re
 import os
 from PyPDF2 import PdfReader
 
+def getTitle(metadata,text):
+    if(metadata.title!="" and metadata.title!="None"):
+        title = metadata.title
+    else:
+        # Use regular expressions to extract the title
+        title_regex = re.compile(r"Title:\s*(.*)")
+        title_match = re.search(title_regex, text)
+        title = title_match.group(1).strip() if title_match else ""
+    
+    return title
+
+def getAuthors(metadata,text):
+    if(metadata.author!="" and metadata.author!="None"):
+        authors = metadata.author
+    else:
+        # Use regular expressions to extract the author
+        authors_regex = re.compile(r"Author\(s\):\s*(.*)")
+        authors_match = re.search(authors_regex, text)
+        authors = authors_match.group(1).strip() if authors_match else ""
+    
+    return authors
+
+def getAbstract(pdf):
+
+    text = pdf.pages[0].extract_text()
+
+    abstract_regex = re.compile(r"Abstract ?.?\.? ?((?:.|\n)*?)\n[1-9I]\.?\s+") # Abstract\.? ?((?:.|\n)*?)\n[1-9A-Z]\.?\s+(?:INTRODUCTION|Introduction)
+    abstract_match = re.findall(abstract_regex, text)
+    abstract = abstract_match.pop() if abstract_match else ""
+
+    return abstract
+
 def extract_pdf_info(file_path):
     """
     Extracts the name of the file, the title of the paper, the authors, and the abstract
@@ -10,23 +42,13 @@ def extract_pdf_info(file_path):
     with open(file_path, "rb") as f:
         # Read the PDF file with PyPDF2's PdfReader
         pdf = PdfReader(f)
+        metadata = pdf.metadata
         text = ""
         for page_num in range(len(pdf.pages)):
             page = pdf.pages[page_num]
             text += page.extract_text()
 
-        # Use regular expressions to extract the title, authors, and abstract
-        title_regex = re.compile(r"Title:\s*(.*)")
-        authors_regex = re.compile(r"Author\(s\):\s*(.*)")
-        abstract_regex = re.compile(r"Abstract ?.?\.? ?((?:.|\n)*?)\n[1-9I]\.?\s+") # Abstract\.? ?((?:.|\n)*?)\n[1-9A-Z]\.?\s+(?:INTRODUCTION|Introduction)
-        title_match = re.search(title_regex, text)
-        title = title_match.group(1).strip() if title_match else ""
-        authors_match = re.search(authors_regex, text)
-        authors = authors_match.group(1).strip() if authors_match else ""
-        abstract_match = re.findall(abstract_regex, text)
-        abstract = abstract_match.pop() if abstract_match else ""
-
-    return (os.path.basename(file_path), title, authors, abstract,text)
+    return (os.path.basename(file_path), getTitle(metadata,text), getAuthors(metadata,text), getAbstract(pdf),text)
 
 def extract_pdf_info_from_directory(directory):
     """
