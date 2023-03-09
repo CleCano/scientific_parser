4418eb6f92707290bfbde9d52a132f70b5d49fff
CleCano <clemhca56@gmail.com>
2023-03-09 11:34:55 +0100

    Amélioration de l'interface graphique

--- graphique.py
+++ graphique.py
@@ -1,42 +1,34 @@
 #!/usr/bin/python3
-
+import os
 from tkinter import *
 from tkinter import filedialog
 import time
 
-fileOut = ""
 fileIn = ""
 def file_open():
    global fileIn
    path = filedialog.askopenfilename(initialdir='~')
-   print(path)
    fileIn = path
-   op = open(path, 'r')
-   read = op.read()
+   exeCommand(fileIn,boolTextXml)
    
+def exeCommand(fileIn,textOrXml):
+   command = ("echo "+textOrXml.get()+" "+fileIn)
+   print(command)
+   os.system(command)
    
 
 
-def folder_open():
-   global fileOut
-   path = filedialog.askdirectory(initialdir='~')
-   print(path)
-   fileOut = path
+
 
 def file_save():
-   files = []
-   if(var_text_option.get()) :
-      
-      files.append(('Text File', '*.txt'))
-   if (var_xml_option.get() ):
-     
-      files.append(('XML File','*.xml'))
-   if(len(files)==0):
-      my_label.config(text="Veuillez cocher ue case pour sauvegarder",foreground="#FF0000")
-   else:
-      my_label.config(text="")
-      file = filedialog.asksaveasfilename(filetypes=files,defaultextension=files)
+
+   if (boolTextXml.get()):files = [("Text File", ".txt")]
+   else : files = files = [("XML File", ".xml")]
+   file = filedialog.asksaveasfilename(filetypes=files,defaultextension=files)
+def on_select():
+   print("Radio button sélectionné",boolTextXml.get())
    
+
 # Créer une fenêtre Tkinter
 window = Tk()
 
@@ -47,15 +39,16 @@ frame_top.pack(side='top', padx=10, pady=10)
 button_open = Button(frame_top, text='Open file', command=file_open)
 button_open.pack(side='left', padx=10)
 
-var_text_option = BooleanVar()
-var_text_option.set(True) 
-checkbox_text_option = Checkbutton(frame_top, text='Text Option', variable=var_text_option)
-checkbox_text_option.pack(side='left', padx=10)
+boolTextXml = BooleanVar()
+boolTextXml.set(True)
+radButton_text_option = Radiobutton(frame_top, text='Text Option', variable=boolTextXml,value=True,command=on_select)
+radButton_xml_option = Radiobutton(frame_top, text='XML Option', variable=boolTextXml,value=False,command=on_select)
 
-var_xml_option = BooleanVar()
-var_xml_option.set(False)
-checkbox_xml_option = Checkbutton(frame_top, text='XML Option', variable=var_xml_option)
-checkbox_xml_option.pack(side='left', padx=10)
+radButton_text_option.pack(side='left', padx=10)
+
+
+
+radButton_xml_option.pack(side='left', padx=10)
 
 # Au milieu : une zone de texte "preview" désactivée
 frame_middle = Frame(window)
@@ -68,17 +61,14 @@ text_preview.pack()
 frame_bottom = Frame(window)
 frame_bottom.pack(side='bottom', padx=10, pady=10)
 
-button_choose_path = Button(frame_bottom, text='Choose path', command=folder_open)
-button_choose_path.pack(side='left', padx=10)
 
-entry_out_name = Entry(frame_bottom, width=30)
-entry_out_name.pack(side='left', padx=10)
 
 button_save = Button(frame_bottom, text='Save', command=file_save)
-button_save.pack(side='right', padx=10)
+button_save.pack(padx=10)
 
 my_label = Label(window, text="")
 my_label.pack()
+
 # Démarrer la boucle principale Tkinter
 window.mainloop()
 
