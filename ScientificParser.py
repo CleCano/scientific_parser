529b7c8c6d0d5a60d0e27e8faf22a795e4fdc87e
CleCano <clemhca56@gmail.com>
2023-05-14 12:51:29 +0200

    Amélioration des auteurs

--- ScientificParser.py
+++ ScientificParser.py
@@ -3,6 +3,7 @@ import re
 import os
 import numpy
 import sys
+import copy
 from PyPDF2 import PdfReader
 
 
@@ -18,7 +19,8 @@ def transformAccent(line):
         "´": {
             "e": "é",
             "a": "á",
-            "E": "É"
+            "E": "É",
+             
         },
         "¨": {
             "a": "ä",
@@ -200,8 +202,8 @@ def getAuthors(metadata,text, title):
         i = 0
         # On commence par virer les lignes inutiles et le titre
         found = False
-        while(not found or ss[i].split("\n")[0].strip() in title):
-            if ss[i].split("\n")[0].strip() in title or title in ss[i]:
+        while(not found or ss[i].split("\n")[0].strip() in title or ss[i].split("\n")[0].split("*")[0].strip() in title):
+            if ss[i].split("\n")[0].strip() in title or title in ss[i] or ss[i].split("\n")[0].split("*")[0].strip() in title: # On regarde avec l'etoile car des fois ils mettent ca pour faire reference a une definition en bas de page
                 found = True
             i += 1
         # regex nom + prenom autheur : 
@@ -252,9 +254,10 @@ def getAuthors(metadata,text, title):
                         lines_read.append(abracadabra)
                         affiliations.append(ss[abracadabra])
                         abracadabra += 1
-
-        author_regex = re.compile(r"((?:(?:[A-Z](?:[a-z]|é|á|è|ç|î|í|à)*)|[A-Z].)(?:(?: [a-z]* )|-| )(?:[A-Z].[A-Z]. |[A-Z]. )?(?:(?:[A-Z]|[a-z]|é|á|è|ç|î|í|à)*(?:-[A-Z](?:[a-z]|é|á|è|ç|î|í|à)*)?))[1-9;]{0,3}")
+        # ((?:(?:[A-Z](?:[a-z]|é|á|è|ç|î|í|à)*)|[A-Z].)(?:(?: [a-z]* )|-| )(?:[A-Z].[A-Z]. |[A-Z]. )?(?:(?:[A-Z]|[a-z]|é|á|è|ç|î|í|à)*(?:-[A-Z](?:[a-z]|é|á|è|ç|î|í|à)*)?)[1-9;∗*†]{0,3})
+        author_regex = re.compile(r"((?:(?:[A-Z](?:[a-z]|é|á|è|ç|î|í|à)*)|[A-Z].)(?:(?: [a-z]* )|-| )(?:[A-Z].[A-Z]. |[A-Z]. )?(?:(?:[A-Z]|[a-z]|é|á|è|ç|î|í|à)*(?:-[A-Z](?:[a-z]|é|á|è|ç|î|í|à)*)?)[1-9;∗*†]{0,3})")
         date_regex = re.compile(r"(Jan(?:uary)?|Feb(?:ruary)?|Mar(?:ch)?|Apr(?:il)?|May|Jun(?:e)?|Jul(?:y)?|Aug(?:ust)?|Sep(?:tember)?|Oct(?:ober)?|Nov(?:ember)?|Dec(?:ember)?)(\s+(\d{1,2}))?,?\s+(\d{4})")
+        ttdclana = ""
         for line in range(i, abstractLine):
             #print(ss[line])
             if author_regex.search(ss[line]):
@@ -268,25 +271,39 @@ def getAuthors(metadata,text, title):
                     isAffiliationOrDate = True
                 if not isAffiliationOrDate:
                     for b in e:
-                        authors.append(b)
+                        # On rajoute un check utile pour les nom prenom de plus de 2 mots
+                        # Pour ce faire on commence par regarder si sur la ligne du nom trouvé avec la prenière regex, s'il y a un autre mot commencant par une majuscule 
+                        # sans qu'il n'y ai de séparateur entre du genre "," ou "and"
+                        # Cependant pour ne pas se faire avoir, dans certains texte quand il y a plusieurs auteurs, et que les derniers sont séparé par le mot and
+                        # il se peut que celui-ci se fasse coller au nom de l'auteur a droite du "and", donc c'est pour cela que l'on ne fait ces étapes uniquement si 
+                        # le nom de l'auteur actuel ne fini pas par and, cela peut poser problème quand le nom de l'auteur fini par and, mais la probabilité comparé à celle-ci
+                        # est largement négligeable
+                        finded = False
+                        maj_regex = re.compile(r"^((?: )?[A-Z].*)")
+                        if not b.endswith("and"): # On ne veut pas ceux qui terminent par and
+                            long_name = ss[line].split(b)[1].split(",")[0] #On coupe sur les virgule pour prendre le texte entre le nom et la virgule
+                            if not long_name.strip().startswith("and"): # On ne veut pas ceux qui commencent par and
+                                if re.match(maj_regex, long_name):
+                                    ttdclana = long_name # txt to dont considere like a nom auteur
+                                    authors.append(b + long_name)
+                                    #print(long_name)
+                                    finded = True
+                                    #print("houlala : ", b + long_name)
+
+                        if not finded and b not in ttdclana:
+                            #print("houlala : ", b)
+                            authors.append(b)
 
         # Récupérer la position de leture des auteurs(et l'exposant dans le cas ou il y en as), afin de les affecté au bonnes affiliation
         
-        print("Titre : ", title)
-        print('Authors : ', authors)
-        print('Emails : ', emails)
-        print('Affiliations : ', affiliations)
+        #print("Titre : ", title)
+        #print('Authors : ', authors)
+        #print('Emails : ', emails)
+        #print('Affiliations : ', affiliations)
 
-        # TODO Déjà première chose, couper les adresses mails, quand elles sont raccourcies
-        #       Pour les mettre ensembles, parcourir les autheurs dans l'ordre qui vont servir de clé a un nouveau dictionnaire
-        #       Ensuite regarder avec une regex si dans leur nom se trouve à la fin une lettre ou un chiffre
-        #       Celle-ci permet d'obtenir l'affiliation correspondante, et donc l'affecter
-        #       Sinon affecter dans l'ordre des affiliations si il ya le mm nombre d'auteurs que d'emails
-        #       Et s'il n'y a qu'une affiliation, alors les mettre pour tous
-        #       Pour email, suffit de regarder si ça matche le nom, sinon affecter dans l'ordre
       
     all_back = {}
-
+    
     #On commence par reformatter la liste d'auteurs pour le moment c'est provisoir, le temps de trouver une solution a la regex
     new_authors = []
     idx_already_taken = []
@@ -312,14 +329,18 @@ def getAuthors(metadata,text, title):
             if date_regex.match(affiliations[i]):
                 affiliations[i] = re.sub(date_regex, '', affiliations[i])
             # On ajoute l'affiliation
-            new_affiliations[newaff] = new_affiliations[newaff]+ " " + affiliations[i]
+            new_affiliations[newaff] = new_affiliations[newaff] + affiliations[i].strip() + " "
             # on l'ajoute à la suite, sauf si cela commence par une minuscule colé à une majuscule, alors on sépare
             if len(affiliations) > i+1 and re.search( r'^[a-z†∗1-9][A-ZÉ]', affiliations[i+1]):
+                #print("new aFFILIATION : ", affiliations[i+1])
                 newaff += 1
                 new_affiliations.append('')
+    #Pour finir le reformattage correctement, on retire tous les espaces de fins
+    for i in range(len(new_affiliations)):
+        new_affiliations[i] = new_affiliations[i].strip()
     affiliations = new_affiliations
     #Truc juste au dessus permet le reformattage des affiliations
-
+    
     # Et ici on reformatte les emails   
     endofemail_regex = re.compile(r"(?:.*)((?:@|Q)[a-zA-Z0-9-.]+\.(?:\n|)[a-z-]+)(?:.*)")  
     new_emails = []
@@ -351,8 +372,9 @@ def getAuthors(metadata,text, title):
         for a in all_back:
             all_back[a]['mail'] = emails[pos].strip()
             pos += 1
-    print("all_back : ", all_back)
 
+    #print("AFF : ", len(affiliations))
+    #print("ALL : ", len(all_back))
     # On parcours enfin les affiliations
     # Si il n'y a qu'une affiliation alors il est probable qu'ils l'aient tous
     if len(affiliations) == 1:
@@ -360,14 +382,70 @@ def getAuthors(metadata,text, title):
             all_back[a]['affiliation'] = affiliations[0]
     # S'il y a autant d'affiliations que de personnes, alors il suffit de les associer
     elif len(affiliations) == len(all_back):
+        #print('EGUALITYT LA HO')
         pos=  0
         for a in all_back:
             all_back[a]['affiliation'] = affiliations[pos]
             pos += 1
-    else:
-        print("AFFILIATIONS NOMBRE : ", len(affiliations))
+    elif len(affiliations) > 1:
+        # Ici on va entrer dans du semi cas par cas SEMI comme les camions
+        #print("camion")
+        # On regarde si les auteurs sont suivient d'une lettre ou d'un charactère sépcial pour les entrer dans une liste
+        liste = [] #C'est la liste
+        regex_exposant = re.compile(r"(.*)([a-z†∗1-9])(;[a-z†∗1-9]){0,2}$")
+        regex_exposants_only = re.compile(r"(?:[a-zA-Z éàÉéáèçîí-])*([a-z†∗1-9])(?:;([a-z†∗1-9])){0,2}(?:,){0,1}$")
+        for a in all_back:
+            #print(" ee : ", a)
+            if re.match(regex_exposant, a): #TODO IL NE COMPREND PAS ICI
+                #print("OHHHHHH ", re.match(regex_exposant, a).group(2), " ", re.match(regex_exposant, a).group(1))
+                affiliation_lettre = []
+                auteur_sans_expo = re.match(regex_exposant, a).group(1)
+                for z in re.match(regex_exposants_only, a).groups():
+                    if not z == None:
+                        affiliation_lettre.append(z)
+                        auteur_sans_expo = auteur_sans_expo.split(z)[0]
+                #print("AFFILIATION LETTRE : ", affiliation_lettre)
+                liste.append({"auteur": auteur_sans_expo, "affiliation_lettre": affiliation_lettre}) #"""re.match(regex_exposant, a).group(2)"""
+        # Maintenant on va compter combien de lettre unique il y a 
+        unique_affiliation_lettre = set()
+        for l in liste:
+            for a in l["affiliation_lettre"]:
+                unique_affiliation_lettre.add(a)
+            #unique_affiliation_lettre.add(l["affiliation_lettre"])
+        #print(unique_affiliation_lettre)
+
+        # Maintenant on sait combien de lettre unique il y a, et ont regarde s'il y a la même quantité d'affiliations
+        if len(unique_affiliation_lettre) == len(affiliations):
+            # S'il y a la meme quantité, alors on les associes mais pour cela, il faut récupérer la lettre d'affiliation
+            dictionnaire = {} # Un dictionnaire cette fois ci pour avoir des perfs de ouf
+            regex_exposant = re.compile(r"([a-z†∗1-9])(.*)$") #TODO JE ne COMPREND PAS PK IL LE COMPREND PAS ESPECE DE DEBILE
+            for a in affiliations:
+                if re.match(regex_exposant, a):
+                    dictionnaire[re.match(regex_exposant, a).group(1)] = re.match(regex_exposant, a).group(2)
+            #print(dictionnaire)
+            # Maintenant on va changer dans all_back les noms d'auteurs pour retirer la lettre de fin de mot
+            # Ce code va pas etre beau a voir et terrible, mais c'est le plus simple a mettre en oeuvre
+            l = 0
+            new_all_back = copy.deepcopy(all_back)
+            for a in new_all_back:
+                all_back[liste[l]["auteur"]] = all_back.pop(a)
+                l += 1
+
+            # Maintenant que l'on a tout en oeuvre, il suffit d'associer
+            for l in liste:
+                first = True
+                for a in l["affiliation_lettre"]:
+                    if first:
+                        all_back[l["auteur"]]["affiliation"] = dictionnaire[a]
+                        first = False
+                    else:
+                        all_back[l["auteur"]]["affiliation"] = all_back[l["auteur"]]["affiliation"] + ", " + dictionnaire[a]
+                #all_back[l["auteur"]]["affiliation"] = dictionnaire[l["affiliation_lettre"]]
+
+        #print("AFFILIATIONS NOMBRE : ", len(affiliations))
 
 
+    #print("all_back : ", all_back)
     return all_back#, emails, affiliations}
 
 def getAbstract(text,file_name=""):
@@ -405,7 +483,9 @@ def getIntroduction(text,file_name=""):
     """
     Extracts the introduction of a scientific paper using a regex
     """
+    
     if(file_name=="b0e5c43edf116ce2909ae009cc27a1546f09.pdf"):
+        print(file_name)
         intro2_regex=r"(?:([1-9]+?(?P<point>\.?).?)|([IVX]*(?P<point2>\.?).?))?\s+?(?:(Introduction(s)?)|(INTRODUCTION(S)?))\s+?\n?(?P<text>(?:.|\n)*?)Background"
     else:
         intro2_regex =r"(?:([1-9]+?(?P<point>\.?).?)|([IVX]*(?P<point2>\.?).?))?\s+?(?:(Introduction(s)?)|(INTRODUCTION(S)?))\s+?\n?(?P<text>(?:.|\n)*?)^(([1-9]+?.?)|([IVX]+\.?\s+?.*))\s+?"
@@ -576,7 +656,7 @@ def writeXML(file_name,output_file_name,text,metadata,pdf):
     Writes all the capital information in a .xml file with an XML layout
     """
     outputXML = "<article>\n"
-    outputXML+="\t<preamble>"+file_name+"</preamble>\n"
+    outputXML+="\t<preamble> "+file_name+"</preamble>\n"
     outputXML+="\t<titre>"+getTitle(metadata,text)+"</titre>\n"
     outputXML+="\t<auteurs>\n"
     auteursInfo = getAuthors(metadata,text,getTitle(metadata,text))
